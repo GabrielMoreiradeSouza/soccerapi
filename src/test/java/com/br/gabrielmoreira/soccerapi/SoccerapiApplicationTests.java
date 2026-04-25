28875a0eb9e875340b1ff326b377c81b99800275
Gabriel Moreira de Souza <120267591+GabrielMoreiradeSouza@users.noreply.github.com>
2026-04-24 21:46:19 -0300

    initial commit

---
+++ src/test/java/com/br/gabrielmoreira/soccerapi/SoccerapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.gabrielmoreira.soccerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoccerapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
