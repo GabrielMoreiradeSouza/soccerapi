083dc136edc98de5e33a46666f83df156c9a09d3
Gabriel Moreira de Souza <120267591+GabrielMoreiradeSouza@users.noreply.github.com>
2026-04-25 18:52:09 -0300

    add logic on repository

--- src/main/java/com/br/gabrielmoreira/soccerapi/repository/JogadorRepository.java
+++ src/main/java/com/br/gabrielmoreira/soccerapi/repository/JogadorRepository.java
@@ -1,10 +1,19 @@
 package com.br.gabrielmoreira.soccerapi.repository;
 
 import com.br.gabrielmoreira.soccerapi.entity.Jogador;
+import org.hibernate.annotations.Parameter;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface JogadorRepository extends JpaRepository<Jogador, Long> {
 
+
+    List<Jogador> findByTime(String time);
+
+    Optional<Jogador> findByNome(@Param("nome") String nome);
 }
--- src/main/java/com/br/gabrielmoreira/soccerapi/service/JogadorService.java
+++ src/main/java/com/br/gabrielmoreira/soccerapi/service/JogadorService.java
@@ -1,4 +1,22 @@
 package com.br.gabrielmoreira.soccerapi.service;
 
+import com.br.gabrielmoreira.soccerapi.entity.Jogador;
+import com.br.gabrielmoreira.soccerapi.repository.JogadorRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class JogadorService {
+
+    private final JogadorRepository jogadorRepository;
+
+    public JogadorService(
+            JogadorRepository jogadorRepository) {
+        this.jogadorRepository = jogadorRepository;
+    }
+
+    public List<Jogador> findByNome(String nome) {
+        return jogadorRepository.findByNome(nome).map(List::of).orElseGet(List::of);
+    }
 }
