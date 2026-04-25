bfec0e6176e1dd5c0ce0f51d1f225c53f2aed997
Gabriel Moreira de Souza <120267591+GabrielMoreiradeSouza@users.noreply.github.com>
2026-04-25 18:24:16 -0300

    add repository

---
+++ src/main/java/com/br/gabrielmoreira/soccerapi/repository/JogadorRepository.java
@@ -0,0 +1,10 @@
+package com.br.gabrielmoreira.soccerapi.repository;
+
+import com.br.gabrielmoreira.soccerapi.entity.Jogador;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JogadorRepository extends JpaRepository<Jogador, Long> {
+
+}
